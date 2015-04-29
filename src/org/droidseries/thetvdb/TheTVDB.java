45c3eb07e30c6ee32d904b759a92040ce05b0c7a
Guillaume <Guillaume@piv>
2015-04-29 19:35:34 +0000

    4.09: Fixed: found some shows without episode names in TheTVDb, which resulted in an empty show being added to the database.

--- src/org/droidseries/thetvdb/TheTVDB.java
+++ src/org/droidseries/thetvdb/TheTVDB.java
@@ -228,11 +228,9 @@ public class TheTVDB {
                 try{
                     Episode episode = parseEpisode( (List<String>)xml_episodes.get(i) );
                     if (episode != null) {
-                        if(episode.getEpisodeName() != null) {
-                            if(!episode.getEpisodeName().equals("")) {
-                                episodes.add(episode);
-                            }
-                        }
+                        if(episode.getEpisodeName() == null || episode.getEpisodeName().equals(""))
+                        	episode.setEpisodeName(" ");
+                        episodes.add(episode);
                     }
                 } catch (Exception e) {
                     Log.e(TAG, "Error gathering the episodes info from the XML file");
